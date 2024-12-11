22830a8f0d6357c7b1ea00ab17a0685b1695f617
sridhar <sridhar4edu@gmail.com>
2024-12-11 23:20:10 +0530

    clean up v.1

--- flight-service/src/main/java/com/flightops/flight/config/CrewClient.java
+++ flight-service/src/main/java/com/flightops/flight/config/CrewClient.java
@@ -1,7 +1,6 @@
 package com.flightops.flight.config;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Configuration;
 
 @FeignClient(name = "crew-management-service")
 public interface CrewClient {
